d4d1b8f39d6cdc664485818dd54fba17e5039258
effigy <jmcobb@gmail.com>
2010-09-28 11:12:23 -0400

    ogre tut

--- src/main.py
+++ src/main.py
@@ -10,7 +10,16 @@ import SampleFramework as sf
 class TutorialApplication(sf.Application):
  
     def _createScene(self):
-        pass
+        sceneManager = self.sceneManager
+        sceneManager.ambientLight = ogre.ColourValue(1,1,1)
+        
+        ent1 = sceneManager.createEntity ("Robot", "robot.mesh")
+        node1 = sceneManager.getRootSceneNode().createChildSceneNode ("RobotNode")
+        node1.attachObject(ent1)
+        
+        ent2 = sceneManager.createEntity("Robot2", "robot.mesh")
+        node2 = sceneManager.getRootSceneNode().createChildSceneNode("RobotNode2", (50,0,0))
+        node2.attachObject(ent2)
 
 
 if __name__ == '__main__':
