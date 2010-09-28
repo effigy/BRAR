e49651033a800961f20ebed3c6e7c271f055bbdd
effigy <jmcobb@gmail.com>
2010-09-28 10:14:58 -0400

    test

---
+++ src/main.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 28, 2010
+
+@author: jcobb
+'''
+
+import ogre.renderer.OGRE as ogre
+import SampleFramework as sf
+ 
+class TutorialApplication(sf.Application):
+ 
+   def _createScene(self):
+       pass
+
+
+if __name__ == '__main__':
+    pass
